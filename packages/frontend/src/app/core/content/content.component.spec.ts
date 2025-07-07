3f57ae410c07407a4739c8eed42b01da51a2092f
Arek <arkadiusz.pabian@gmail.com>
2025-07-07 21:44:31 +0200

    test: fix content component tests

--- packages/frontend/src/app/core/content/content.component.spec.ts
+++ packages/frontend/src/app/core/content/content.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
+import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
+import { provideRouter } from '@angular/router'
 import { ContentComponent } from './content.component'
 
 describe('ContentComponent', () => {
@@ -9,6 +12,11 @@ describe('ContentComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ContentComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(ContentComponent)
