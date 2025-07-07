6d569d6e8d373e5daff4128bbb2f2d9c6fd3d041
Arek <arkadiusz.pabian@gmail.com>
2025-07-07 20:21:12 +0200

    test: fix test for autofocus-after-init directive

--- packages/frontend/src/app/directives/autofocus-after-init.directive.spec.ts
+++ packages/frontend/src/app/directives/autofocus-after-init.directive.spec.ts
@@ -1,8 +1,34 @@
-import { AutofocusAfterInitDirective } from './autofocus-after-init.directive';
+import { Component, DebugElement } from '@angular/core'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { AutofocusAfterInitDirective } from './autofocus-after-init.directive'
+
+@Component({
+  imports: [AutofocusAfterInitDirective],
+  template: `<input type="text" appAutofocusAfterInit />`,
+})
+class TestHostComponent {}
 
 describe('AutofocusAfterInitDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutofocusAfterInitDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+  let fixture: ComponentFixture<TestHostComponent>
+  let inputEl: DebugElement
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TestHostComponent],
+    })
+
+    fixture = TestBed.createComponent(TestHostComponent)
+    fixture.detectChanges()
+
+    inputEl = fixture.debugElement.query(By.css('input'))
+  })
+
+  it('should focus the input after view init (microtask)', async () => {
+    const focusSpy = spyOn(inputEl.nativeElement, 'focus')
+
+    await Promise.resolve()
+
+    expect(focusSpy).toHaveBeenCalled()
+  })
+})
